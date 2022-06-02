a0874efc1ddd267d52189da1a114f8766d44ed63
--add <--add>
2022-06-02 14:17:31 +0600

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import createCache from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
+// import { StoreProvider } from '../utils/Store';
+// import { SnackbarProvider } from 'notistack';
+// import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createCache({ key: 'css' });
+
+function MyApp({ Component, pageProps, emotionCache = clientSideEmotionCache }) {
+  return (
+  <CacheProvider value={emotionCache}>
+    <Component {...pageProps} />
+    
+  </CacheProvider>)
 }
 
 export default MyApp
